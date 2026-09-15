260f16891abf4e8387454cfb062cda30bc8d1565
Raka Ahmad Abrisam <rakaahmadabrisam@gmail.com>
2026-09-15 17:01:54 +0700

    Update
    + Menambahkan assets sementara

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,50 +1,43 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMovement : MonoBehaviour
+public class MovementController : MonoBehaviour
 {
-    [Header("Pengaturan Gerak")]
-    public float moveSpeed = 5f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f;
+    public float jumpForce = 7f;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadius = 0.2f;
 
     private Rigidbody2D rb;
+    private bool isGrounded;
     private float horizontalInput;
-    private bool isFacingRight = true;
 
     void Start()
     {
-        // Mengambil komponen Rigidbody2D yang terpasang pada karakter
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-        // Mengecek arah untuk membalikkan hadap karakter
-        if (horizontalInput > 0 && !isFacingRight)
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            Flip();
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
-        else if (horizontalInput < 0 && isFacingRight)
-        {
-            Flip();
-        }
+
+        if (horizontalInput > 0) transform.localScale = new Vector3(1, 1, 1);
+        else if (horizontalInput < 0) transform.localScale = new Vector3(-1, 1, 1);
     }
 
     void FixedUpdate()
     {
-        // Mengaplikasikan kecepatan pada sumbu X, membiarkan sumbu Y (gravitasi) tetap normal
-        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
-    }
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
 
-    private void Flip()
-    {
-        // Membalikkan status arah hadap
-        isFacingRight = !isFacingRight;
-
-        // Membalikkan skala X pada Transform
-        Vector3 localScale = transform.localScale;
-        localScale.x *= -1f;
-        transform.localScale = localScale;
+        rb.linearVelocity = new Vector2(horizontalInput * currentSpeed, rb.linearVelocity.y);
     }
 }
